c462af71b0c401c997a63b6d07866b8693991242
Bharat Mukheja <bkmukhej@ncsu.edu>
2016-12-17 01:38:11 -0500

    Problem 2 solved

--- 2.py
+++ 2.py
@@ -2,43 +2,38 @@
 class ListNode(object):
     def __init__(self, x):
         self.val = x[0]
+        self.next = None
         if len(x)>1:
-            for item in x[1:]:
-                self.next = ListNode(item,self)
+            new_node = ListNode(x[1:])
+            self.next = new_node
 
-    def __init__(self, x, prevNode):
-        self.val = x
-        self.next = prevNode
-
+    def __repr__(self):
+        lst = []
+        temp = self
+        while(temp.next!=None):
+            lst.append(temp.val)
+            temp = temp.next
+        return str(lst)
 
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        first = ListNode(l1)
-        second = ListNode(l2)
-        sum = first.val + second.val
-        new_node = ListNode(sum % 10)
-        third = new_node
-        carry_forward = int(sum / 10)
-        while (first.next != None and second.next != None):
-            first = first.next
-            second = second.next
-            sum = first.val + second.val + carry_forward
-            new_node = ListNode(sum % 10, new_node)
-            carry_forward = int(sum / 10)
-        if (first.next == None and second.next != None):
-            while (second.next != None):
-                second = second.next
-                sum = second.val + carry_forward
-                new_node = ListNode(sum % 10, new_node)
-                carry_forward = int(sum / 10)
-        elif (first.next != None and second.next == None):
-            while (first.next != None):
-                first = first.next
-                sum = second.val + carry_forward
-                new_node = ListNode(sum % 10, new_node)
-                carry_forward = int(sum / 10)
-        elif (first.next == None and second.next == None):
-            return third
+        carry_forward = 0
+        carry_forward, val = divmod(l1.val + l2.val + carry_forward, 10)
+        new_node = third = ListNode(val)
+        l1 = l1.next
+        l2=l2.next
+        while (l1 or l2 or carry_forward):
+            val1 = val2 = 0
+            if l1:
+                val1 = l1.val
+                l1 = l1.next
+            if l2:
+                val2 = l2.val
+                l2 = l2.next
+            carry_forward, val = divmod(val1 + val2 + carry_forward,10)
+            new_node.next = ListNode(val)
+            new_node = new_node.next
+        return third
 
 
 solution = Solution()
